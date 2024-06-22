73fe678b3fdbb853a050313a1ad2925e0421f4d7
Cloe <decodtscloe@gmail.com>
2024-06-22 18:42:03 +0200

    ajustement homepage

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ const navLinks = document.querySelector(".nav-links");
 menuBurger.addEventListener('click', () => {
 navLinks.classList.toggle("mobile-menu");
 });
+
+// Carroussel presentation
+const images = document.querySelectorAll('.container img');
+const rightBtn = document.getElementById('right-btn');
+const leftBtn = document.getElementById('left-btn');
+
+let position = 0;
+
+const moveRight = () => {
+    images[position].classList.remove('active');
+    position = (position + 1) % images.length;
+    images[position].classList.add('active');
+}
+
+const moveLeft = () => {
+    images[position].classList.remove('active');
+    position = (position - 1 + images.length) % images.length;
+    images[position].classList.add('active');
+}
+
+rightBtn.addEventListener("click", moveRight);
+leftBtn.addEventListener("click", moveLeft);
+
+// Initially show the first image
+images[position].classList.add('active');
